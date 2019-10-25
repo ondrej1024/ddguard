0fe2feaeb1654489a56aff0132dcb81db74e81b0
Ondrej Wisniewski <ondrej.wisniewski@gmail.com>
2019-10-25 12:06:37 +0200

    - Add handling of parameters from configuration file
    - Add handling of BGL status codes
    - Add handling of display colors according to limits

---
+++ HISTORY.md
@@ -0,0 +1,15 @@
+# VERSION HISTORY
+
+### 0.1 [26/09/2019]
+
+First public release
+
+### 0.2 [24/10/2019]
+
+- Add handling of parameters from configuration file
+- Add handling of BGL status codes
+- Add handling of display colors according to limits
+
+
+
+ 
--- ddguard.py
+++ ddguard.py
@@ -4,20 +4,35 @@
 #  Diabetes Data Guard (DD-Guard): Gateway module
 #  
 #  Description:
+#
 #    The DD-Guard gateway software periodically receives real time data from 
 #    the Minimed 670G pump and uploads it to the cloud service
 #  
 #  Dependencies:
+#
 #    DD-Guard uses the Python driver by paazan for the "Contour Next Link 2.4" 
 #    radio bridge to the Minimed 670G to download real time data from the pump.
 #    https://github.com/pazaan/decoding-contour-next-link
+#    
+#    For the cloud connection and app communication the official Blynk Python
+#    library is used.
+#    https://github.com/blynkkk/lib-python
 #
 #  Author:
+#
 #    Ondrej Wisniewski (ondrej.wisniewski *at* gmail.com)
 #  
-#  Last modified:
-#    23/09/2019
+#  Changelog:
+#
+#    23/09/2019 - Initial public release
+#    13/10/2019 - Add handling of parameters from configuration file
+#    24/10/2019 - Add handling of BGL status codes
+#    24/10/2019 - Add handling of display colors according to limits
 # 
+#  TODO:
+#    - Run timer function as asynchronous thread
+#    - Add some notification mechanism for alarms e.g. Telegram message
+#
 #  Copyright 2019, Ondrej Wisniewski 
 #  
 #  This file is part of the DD-Guard project.
@@ -43,10 +58,11 @@ import time
 import signal
 import syslog
 import sys
+import ConfigParser
 import read_minimed_next24
 
 
-VERSION = "0.1"
+VERSION = "0.2"
 
 UPDATE_INTERVAL = 300
 MAX_RETRIES_AT_FAILURE = 3
@@ -56,7 +72,7 @@ VPIN_SENSOR  = 1
 VPIN_BATTERY = 2
 VPIN_UNITS   = 3
 VPIN_ARROWS  = 4
-VPIN_TIME    = 5
+VPIN_STATUS  = 5
 
 # color definitions
 BLYNK_GREEN  = "#23C48E"
@@ -64,14 +80,89 @@ BLYNK_BLUE   = "#04C0F8"
 BLYNK_YELLOW = "#ED9D00"
 BLYNK_RED    = "#D3435C"
 
+# Special BGL values
+BGL_VALUE_TOOLOW    = 777
+BGL_VALUE_CHANGE    = 773
+BGL_VALUE_UPDATING  = 771
+BGL_VALUE_CALIBRATE = 770
+BGL_VALUE_WAITING   = 769
 
-BLYNK_AUTH =  # insert your Auth Token here
-BLYNK_HEARTBEAT = 30
+BGL_MSG_TOOLOW    = "Sensor value low"
+BGL_MSG_CHANGE    = "Change sensor"
+BGL_MSG_UPDATING  = "Updating sensor"
+BGL_MSG_CALIBRATE = "Calibrate sensor"
+BGL_MSG_WAITING   = "Waiting for sensor"
 
-blynk = blynklib.Blynk(BLYNK_AUTH, heartbeat=BLYNK_HEARTBEAT)
-timer = blynktimer.Timer()
+bgl_status_codes  = {BGL_VALUE_TOOLOW:   BGL_MSG_TOOLOW,
+                     BGL_VALUE_CHANGE:   BGL_MSG_CHANGE,
+                     BGL_VALUE_UPDATING: BGL_MSG_UPDATING,
+                     BGL_VALUE_CALIBRATE:BGL_MSG_CALIBRATE,
+                     BGL_VALUE_WAITING:  BGL_MSG_WAITING
+                    }
 
 is_connected = False
+
+CONFIG_FILE = "/etc/ddguard.conf"
+
+
+#########################################################
+#
+# Function:    read_config()
+# Description: Read parameters from config file
+# 
+#########################################################
+def read_config(cfilename):
+   
+   # Parameters from global config file
+   config = ConfigParser.ConfigParser()
+   config.read(cfilename)
+
+   try:
+      # Read Blynk parameters
+      read_config.blynk_token     = config.get('blynk', 'token').strip('"').strip("'").split("#")[0]
+      read_config.blynk_heartbeat = int(config.get('blynk', 'heartbeat').strip('"').strip("'").split("#")[0])
+   except ConfigParser.NoOptionError, ConfigParser.NoSectionError:
+      syslog.syslog(syslog.LOG_ERR, "ERROR - Needed blynk option not found in config file")
+      return False
+
+   try:
+      # Read BGL parameters
+      read_config.bgl_low_val      = int(config.get('bgl', 'bgl_low').strip('"').strip("'").split("#")[0])
+      read_config.bgl_pre_low_val  = int(config.get('bgl', 'bgl_pre_low').strip('"').strip("'").split("#")[0])
+      read_config.bgl_pre_high_val = int(config.get('bgl', 'bgl_pre_high').strip('"').strip("'").split("#")[0])
+      read_config.bgl_high_val     = int(config.get('bgl', 'bgl_high').strip('"').strip("'").split("#")[0])
+   except ConfigParser.NoOptionError, ConfigParser.NoSectionError:
+      syslog.syslog(syslog.LOG_ERR, "ERROR - Needed bgl option not found in config file")
+      return False
+
+   print ("Blynk token:  %s" % read_config.blynk_token)
+   print ("BGL low:      %d" % read_config.bgl_low_val)
+   print ("BGL pre low:  %d" % read_config.bgl_pre_low_val)
+   print ("BGL pre high: %d" % read_config.bgl_pre_high_val)
+   print ("BGL high:     %d" % read_config.bgl_high_val)
+   return True
+
+    
+#########################################################
+#
+# Function:    on_sigterm()
+# Description: signal handler for the TERM and INT signal
+# 
+#########################################################
+def on_sigterm(signum, frame):
+
+   blynk.disconnect()
+   syslog.syslog(syslog.LOG_NOTICE, "Exiting DD-Guard daemon")
+   sys.exit()
+
+
+# read configuration parameters
+if read_config(CONFIG_FILE) == False:
+   sys.exit()
+
+# Init Blynk instance
+blynk = blynklib.Blynk(read_config.blynk_token, heartbeat=read_config.blynk_heartbeat)
+timer = blynktimer.Timer()
 
 @blynk.handle_event("connect")
 def connect_handler():
@@ -100,6 +191,7 @@ def disconnect_handler():
 #########################################################
 @timer.register(interval=5, run_once=True)
 @timer.register(interval=UPDATE_INTERVAL, run_once=False)
+# TODO: Run this as separate thread so we don't cause ping timeouts
 def send_pump_data():
    
     print "read data from pump"
@@ -114,41 +206,64 @@ def send_pump_data():
           syslog.syslog(syslog.LOG_ERR, "Unexpected ERROR occured")
           pumpData = None
           numRetries -= 1
-          time.sleep(5)
+          if numRetries > 0:
+             time.sleep(5)
     
     if pumpData != None:
        print "send data to cloud backend"
+       
        # Set sensor data
-       blynk.virtual_write(VPIN_SENSOR,  pumpData["bgl"] if pumpData["bgl"] != 770 else None)
-       blynk.virtual_write(VPIN_ARROWS, pumpData["trend"])
-       blynk.virtual_write(VPIN_TIME, str(pumpData["time"]).split(' ')[1].split('.')[0])
-       blynk.set_property(VPIN_TIME, "color", BLYNK_GREEN)
+       if pumpData["bgl"] in bgl_status_codes:
+          # Special status code
+          blynk.virtual_write(VPIN_SENSOR, None)
+          blynk.virtual_write(VPIN_ARROWS, "--")
+          blynk.virtual_write(VPIN_STATUS, bgl_status_codes[pumpData["bgl"]])
+          blynk.set_property(VPIN_STATUS, "color", BLYNK_RED)
+       else:
+          # Regular BGL data
+          blynk.virtual_write(VPIN_SENSOR, pumpData["bgl"])
+          if pumpData["bgl"] < read_config.bgl_low_val:
+             blynk.set_property(VPIN_SENSOR, "color", BLYNK_RED)
+          elif pumpData["bgl"] < read_config.bgl_pre_low_val:
+             blynk.set_property(VPIN_SENSOR, "color", BLYNK_YELLOW)
+          elif pumpData["bgl"] < read_config.bgl_pre_high_val:
+             blynk.set_property(VPIN_SENSOR, "color", BLYNK_GREEN)
+          elif pumpData["bgl"] < read_config.bgl_high_val:
+             blynk.set_property(VPIN_SENSOR, "color", BLYNK_YELLOW)
+          else:
+             blynk.set_property(VPIN_SENSOR, "color", BLYNK_RED)             
+          blynk.virtual_write(VPIN_ARROWS, pumpData["trend"])
+          blynk.virtual_write(VPIN_STATUS, str(pumpData["time"]).split(' ')[1].split('.')[0])
+          blynk.set_property(VPIN_STATUS, "color", BLYNK_GREEN)
+       
        # Set pump data
        blynk.virtual_write(VPIN_BATTERY, pumpData["batt"])
+       if pumpData["batt"] <= 25:
+          blynk.set_property(VPIN_BATTERY, "color", BLYNK_RED)
+       elif pumpData["batt"] <= 50:
+          blynk.set_property(VPIN_BATTERY, "color", BLYNK_YELLOW)
+       else:
+          blynk.set_property(VPIN_BATTERY, "color", BLYNK_GREEN)
        blynk.virtual_write(VPIN_UNITS,   pumpData["unit"])
+       if pumpData["unit"] <= 25:
+          blynk.set_property(VPIN_UNITS, "color", BLYNK_RED)
+       elif pumpData["unit"] <= 75:
+          blynk.set_property(VPIN_UNITS, "color", BLYNK_YELLOW)
+       else:
+          blynk.set_property(VPIN_UNITS, "color", BLYNK_GREEN)
     else:
        syslog.syslog(syslog.LOG_ERR, "Unable to get data from pump")
        # Clear sensor data
-       blynk.virtual_write(VPIN_SENSOR, None)
-       blynk.virtual_write(VPIN_ARROWS, 0)
-       blynk.set_property(VPIN_TIME, "color", BLYNK_RED)
+       #blynk.virtual_write(VPIN_SENSOR, None)
+       blynk.set_property(VPIN_SENSOR, "color", BLYNK_GREEN)
+       #blynk.virtual_write(VPIN_ARROWS, 0)
+       # TODO: set time of last received data
+       blynk.set_property(VPIN_STATUS, "color", BLYNK_RED)
        # Clear pump data
-       blynk.virtual_write(VPIN_BATTERY, 0)
-       blynk.virtual_write(VPIN_UNITS, 0)
+       #blynk.virtual_write(VPIN_BATTERY, 0)
+       #blynk.virtual_write(VPIN_UNITS, 0)
        
     print
-    
-#########################################################
-#
-# Function:    on_sigterm()
-# Description: signal handler for the TERM and INT signal
-# 
-#########################################################
-def on_sigterm(signum, frame):
-
-   blynk.disconnect()
-   syslog.syslog(syslog.LOG_NOTICE, "Exiting DD-Guard daemon")
-   sys.exit()
 
 
 ##########################################################           
