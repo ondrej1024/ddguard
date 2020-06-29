0880eea7ba280d4a066960a8050105cdac90b7d2
gdave44 <gdave44@gmail.com>
2020-06-29 16:07:37 -0600

    Update ddguard.py

--- ddguard.py
+++ ddguard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
 ###############################################################################
 #  
 #  Diabetes Data Guard (DD-Guard): Gateway module
